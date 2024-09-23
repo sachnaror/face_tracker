98330e7a23e3690eee3072c7dc694647969ab775
Sachin <sachnaror@gmail.com>
2024-09-23 06:10:14 +0530

    Diligence unlocks the doors of wisdom

--- config/firebase_config.py
+++ config/firebase_config.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, firestore, storage
 # Path to your Firebase Admin SDK JSON file
 cred = credentials.Certificate("/Users/homesachin/Desktop/zoneone/practice/face_tracker/config/firebase-adminsdk.json")
 firebase_admin.initialize_app(cred, {
-    'storageBucket': 'wer345345c4ertwtr.appspot.com'  # Firebase Storage bucket
+    'storageBucket': 'wer34715c4ertwtr.appspot.com'  # Firebase Storage bucket
 })
 
 # Firestore and storage references
